58cc62af4e8dc0e87925e290883ca4523bdbab15
cngo2512 <49543131+cngo2512@users.noreply.github.com>
2019-04-11 21:40:21 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Player
+{
+    int position;
+    string name;
+    int numGuessed;
+public:
+    void rollDice
+    {
+        
+    }
+    void guessRoll
+    {
+        
+    }
+    void movePosition
+    {
+        
+    }
+    void printBoard
+    {
+        int board[10][10];
+        
+    }
+}
+
+class Computer: public Player
+{
+    
+};
+
+class 
+
+int main()
+{
+    char c;
+    c = 56;
+    cout << c;
+    
+    system("pause");
+    return 0;
+}
