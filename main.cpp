2989bd86e55ddc1548656666afdded868aa46c33
cngo2512 <49543131+cngo2512@users.noreply.github.com>
2019-04-16 15:39:44 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,41 +5,83 @@ using namespace std;
 
 class Player
 {
+public:
     int position;
+    int dice;
+    int playernum;
     string name;
     int numGuessed;
-public:
-    void rollDice
-    {
+    int Instructions () {
+        while (playernum != 1 || playernum != 2) {
+            cout << "Welcome! How many players? (1 or 2): ";
+            cin >> playernum;
         
+        }
+        return playernum;
     }
-    void guessRoll
-    {
-        
+    
+    int rollDice() {
+        srand(time(NULL));//set up for random number
+        int max = 6;
+        dice = rand() % max + 1;
+        cout << "The dice has been rolled." << endl;
+        return dice;
     }
-    void movePosition
-    {
-        
+    
+    void Playerguess () {
+        cout << "What is your guess?";
+        cin >> numGuessed;
     }
-    void printBoard
-    {
+    
+    void guessRoll (int dice) {
+        if (numGuessed == dice) {
+        movePosition(dice);
+        }
+    }
+    
+    void movePosition (int dice) {
+        position += dice;
+    }
+    
+    void printBoard() {
         int board[10][10];
-        
     }
-}
+};
 
 class Computer: public Player
 {
+public:
+    Computer()
+    {
+        name = "Computer";
+        position = 0;
+    }
+    
     
 };
-
-class 
 
 int main()
 {
     char c;
     c = 56;
     cout << c;
+    
+    Player player1;
+    if (player1.Instructions() == 1) {
+        Computer Comp;
+        player1.dice = player1.rollDice();
+        player1.Playerguess();
+        cout << "Player 1 guessed: " << player1.numGuessed;
+        
+    }
+    else {
+        player1.dice = player1.rollDice();
+        player1.Playerguess();
+        cout << "Player 1 guessed: " << player1.numGuessed;
+        Player player2;
+        player2.Playerguess();
+        cout << "Player 2 guessed: " << player2.numGuessed;
+    }
     
     system("pause");
     return 0;
